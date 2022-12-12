8fdc7ece40b89518b1aaac6b5bc1656641158ab3
ezana3 <ezanategene@gmail.com>
2022-12-12 01:34:21 -0800

    Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main- prints a given phrase
+ * return- 0 if exited properly othere numbers otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
