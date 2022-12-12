33c2548e1e72a980a24a354c675fa5443a76c1b4
ezana3 <ezanategene@gmail.com>
2022-12-12 01:47:30 -0800

    fixed ;

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * return- 0 if exited properly othere numbers otherwise
  */
 
-int main(void)
+int main(void) /*prints phrase*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
