f3e1ec4cb866cd3351d3e4aebc4eec14661a55ba
ezana3 <ezanategene@gmail.com>
2022-12-12 01:42:15 -0800

    fixed;

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
 /**
- * main - prints a given phrase
+ * main - prints proper grammer,
+ * but the outcome is a piece of art,
  * return- 0 if exited properly othere numbers otherwise
  */
 
