5ad7ffb040a7852fc3f4ff8f3be07f1446f5288d
ezana3 <ezanategene@gmail.com>
2022-12-12 01:53:18 -0800

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the phrase proper grammer, but the outcome is a piece of art,
- * return: 0 if exited properly othere numbers otherwise
+ * return : 0 if exited properly othere numbers otherwise
  */
 
 int main(void) 
