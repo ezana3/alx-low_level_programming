bd84860a083350d6ccca354c8607e2c172f47b8d
ezana3 <ezanategene@gmail.com>
2022-12-12 00:47:08 -0800

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main-prints a given phrase
+ * Return: 0 if exitd properly, non-zero otherwise
+ * /
+
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
 }
