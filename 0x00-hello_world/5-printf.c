6ed7744ecb06ec610f3c24f16d8e796192f65d22
ezana3 <ezanategene@gmail.com>
2022-12-12 01:52:14 -0800

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints the phrase proper grammer, but the outcome is a piece of art,
- * return- 0 if exited properly othere numbers otherwise
+ * return: 0 if exited properly othere numbers otherwise
  */
 
-int main(void) /*prints phrase*/
+int main(void) 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
