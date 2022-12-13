a8049d3917204dca94aabcc461ea413deb200d47
ezana3 <ezanategene@gmail.com>
2022-12-12 16:32:08 -0800

    fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ int main(void)
 	srandlj(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
-	if (a > 5)
+	if (x > 5)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, x);
 	}
-	else if (a == 0)
+	else if (x == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
