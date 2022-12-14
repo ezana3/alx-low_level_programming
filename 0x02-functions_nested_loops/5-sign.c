6e061e86d57e5bd79b76ab83f04a2ca87e1ad1bb
ezana3 <ezanategene@gmail.com>
2022-12-14 05:53:29 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign -> prints the sign of a number.
+ * @n: character
+ * Return: always 0;
+ */
+
+int print_sign(int n);
+{
+	if (n < 1)
+	{
+		_putchar(-);
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar(+);
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
