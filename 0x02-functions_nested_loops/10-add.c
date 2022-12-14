6b908a5761a0822af923f844c89949d584c279e0
ezana3 <ezanategene@gmail.com>
2022-12-14 10:31:58 -0800

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * add - adds.
+ * main - function.
+ * Return - Always 0;
+ */
+
+int main(void)
+{
+	int add(int x, int y)
+	{
+		int n;
+
+		n = add(x, y);
+		printf("%d\n", n);
+		return (0);
+	}
+}
