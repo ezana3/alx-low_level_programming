c0cfd4c0a2f1c71636dd641786865b1edeca06ae
ezana3 <ezanategene@gmail.com>
2022-12-14 07:31:12 -0800

    fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,14 @@
  * @x: character.
  * Return: Always 0;
  */
-int print_last_digit(int x)
+int print_last_digit(int n)
 {
-	int n;
+	int x;
 
 	if (n < 0)
 	{
 		n = -n;
-		n = x % 10;
+		x = n % 10;
 		_putchar(x + '0');
 		return (n % 10);
 	}
