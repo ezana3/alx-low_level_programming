47ccfc73feb193370e59d23ac650687657c60fd8
ezana3 <ezanategene@gmail.com>
2022-12-14 01:48:34 -0800

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints alphabets
+ *
+ * Return: Always 0;
+ */
+void print_alphabet(void);
+{
+	int x;
+
+	for (x = a; x <= z; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
