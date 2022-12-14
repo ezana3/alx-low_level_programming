30c5a8e4a4d53c86b86f0a53a9a18829686e1b84
ezana3 <ezanategene@gmail.com>
2022-12-14 06:53:27 -0800

    lasd digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit.
+ * @x: character.
+ * Return: Always 0;
+ */
+int print_last_digit(int x);
+{
+	for
+	{
+		x = ('x%10');
+		return (x);
+	}
+}
