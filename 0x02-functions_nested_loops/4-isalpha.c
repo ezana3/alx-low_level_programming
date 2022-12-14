1cd27649373aba55693c0cbe7d2000aa5763ee9b
ezana3 <ezanategene@gmail.com>
2022-12-14 05:30:49 -0800

    is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * isalpha -> Prints alphabetic character.
+ * @c:  is Character.
+ * Return: Always zero;
+ */
+
+int _isalpha(int c);
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
