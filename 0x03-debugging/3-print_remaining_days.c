3af7119d5562c387ace97046b69d3ec5683107ad
ezana3 <ezanategene@gmail.com>
2022-12-16 03:37:08 -0800

    fixed

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,6 +9,7 @@
 * @year: year
 * Return: void
 */
+
 void print_remaining_days(int month, int day, int year)
 {
 if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
@@ -30,5 +31,6 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
+}
 }
 }
