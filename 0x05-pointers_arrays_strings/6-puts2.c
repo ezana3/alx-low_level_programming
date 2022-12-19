b6e57e215b3e7812fe276154988807b164750ee6
ezana3 <ezanategene@gmail.com>
2022-12-19 08:33:32 -0800

    fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i++)
+	for (; str[i] != '\0'; i++);
 	{
 		if ((i % 2) == 0)
 			_putchar(str[i]);
 		else
 			continue;
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
