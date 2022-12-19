587145cd840b298a9de66b71865d27ca506f5806
ezana3 <ezanategene@gmail.com>
2022-12-19 08:21:29 -0800

    reverse stirng

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses stirng
+ * @s: string
+ * Return: string in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int c = 0;
+
+	while (s[c] != '\n')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
+}
